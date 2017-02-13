eaa7eebbb25e1b922bddf58b8902c9a9ac5c4e0d
daredevilek <m.czyz.j@gmail.com>
2017-02-13 15:39:03 +0100

    ui first framework

--- ui.R
+++ ui.R
@@ -6,17 +6,33 @@ shinyUI(fluidPage(theme = 'bootstrap.css',
   # Sidebar with a slider input for number of bins
   sidebarLayout(position = 'left',
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30),
+      radioButtons(inputId = 'analyseType',
+                   label = 'Wybierz typ analizy',
+                   choices = list('Prawdopodobieństwo wykrycia' = "pVal",
+                                  'Min. wielkość próby' = 'mWP',
+                                  'Graniczny poziom infekcyjności' = 'gPI')
+                         ),
+      sliderInput(inputId = "probaB",
+                  label = "Prawdopodobieństwo wykrycia",
+                  min = 0.01,
+                  max = 1.00,
+                  value = 0.75),
+      sliderInput(inputId = "infR",
+                  label = "Poziom infekcji",
+                  min = 0.0001,
+                  max = 0.50,
+                  value = 0.01),
+      numericInput(InputId = 'minNumber',
+                   label = 'Minimalna liczebność próby', min = 200),
+      numericInput(InputId = 'maxNumber',
+                   label = 'Maksymalna liczebność próby', max = 4000),
+      
     br(),
     p('Aplikacja zbudowana w', 
       a('Shiny', href = 'http://www.rstudio.com/shiny'), 'dla', 
       a('R Studio', href = 'http://www.rstudio.com')),
     p('na podstawie', a('kodu Renke Luekhen.', 
-                        href = 'https://rstudio-pubs-static.s3.amazonaws.com/98526_15da936c6bab41c6bc4ccd0fbbf2cfb1.html'))
+                        href = 'https://goo.gl/FopD9R'))
     ),
 
     # Show a plot of the generated distribution
