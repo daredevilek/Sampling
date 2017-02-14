5ba6427d5700bcb70bcd1b2a6745e55b9fd4f5e7
daredevilek <m.czyz.j@gmail.com>
2017-02-14 11:45:51 +0100

    small adjustments - added sensitivity

--- server.R
+++ server.R
@@ -3,30 +3,24 @@ library(pracma) # library for the function nthroot
 library(plyr)
 library(ggplot2)
 
-cz1 <- c(0.9) #czulosc
+#cz1 <- c(0.9) #czulosc
 
 
 shinyServer(function(input, output, session) {
 
   #prawdopodobienstwo wykrycia
-# cz1 <- reactive({
-# cz1 <- c(input$czT) #czulosc
-# })
-  pr <- function(){
-    n1 <- seq(input$minNumber, input$maxNumber)
-    p1 <- 1-(1-input$infR*cz1)^n1
-    list(pe1 = p1, ne1 = n1)
+  probability1 <- function(){
+    sampleN1 <- seq(input$minNumber, input$maxNumber)
+    probab1 <- 1-(1-input$infR*input$czT)^sampleN1
+    list(pe1 = probab1, ne1 = sampleN1)
   }
   
   #minimalna wielkosc proby do wykrycia danego poziomu infekcyjnosci
   #z danym prawdopodobienstwem
-  #inf2<-seq(0.001,0.003,0.001) # liczba infekcji
-  #p2<-0.75 # prawdopodobienstwo wykrycia
-  #n2 = log(1-P1)/log(1-r)
-  #p3<-0.8 # detection probability
-  #n3 = log(1-P2)/log(1-r)
-  #p4<-0.9
-  #n4 = log(1-P3)/log(1-r)
+  #smp <- function(){
+  # infRa <- seq(input$minInf, input$maxInf)
+  # sampleN2 <- log(1-input$probaB)/log(log1-infRa)
+  #}
   
   #poziom inekcyjnosci wykrywany przy danej liczebosci proby
   #i danym prawdopodobienstwie wykrycia
@@ -37,10 +31,11 @@ shinyServer(function(input, output, session) {
   #upInf_2<-(1-(nthroot((1-p6),n5)))*1000
   
   output$wykres <- renderPlot({
-    plot(pr()$pe1~pr()$ne1,
+    plot(probability1()$pe1~probability1()$ne1,
          ylim = c(0,1),
          xlab = 'Wielkość próby',
          ylab = 'Prawdopodobieństwo wykrycia',
          type = 'l')
+    abline(h = 0.95, col = 'red', lty = c(3))
   })
 })
