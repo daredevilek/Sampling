021bc99c43fb60261940f91aca9d94be33eca005
daredevilek <m.czyz.j@gmail.com>
2017-02-14 10:56:03 +0100

    first working v - probability of detection

--- server.R
+++ server.R
@@ -1,42 +1,46 @@
 library(shiny)
 library(pracma) # library for the function nthroot
+library(plyr)
+library(ggplot2)
+
+cz1 <- c(0.9) #czulosc
 
 
 shinyServer(function(input, output, session) {
 
   #prawdopodobienstwo wykrycia
-  n1 <- c(400,600,800) # wielkosc probki
-  cz1 <- c() #czulosc
-  inf1 < -c()/cz # infekcje
-  p1 <- 1-(1-r1*a)^N
+# cz1 <- reactive({
+# cz1 <- c(input$czT) #czulosc
+# })
+  pr <- function(){
+    n1 <- seq(input$minNumber, input$maxNumber)
+    p1 <- 1-(1-input$infR*cz1)^n1
+    list(pe1 = p1, ne1 = n1)
+  }
   
   #minimalna wielkosc proby do wykrycia danego poziomu infekcyjnosci
   #z danym prawdopodobienstwem
-  inf2<-seq(0.001,0.003,0.001) # liczba infekcji
-  p2<-0.75 # prawdopodobienstwo wykrycia
-  n2 = log(1-P1)/log(1-r)
-  p3<-0.8 # detection probability
-  n3 = log(1-P2)/log(1-r)
-  p4<-0.9
-  n4 = log(1-P3)/log(1-r)
+  #inf2<-seq(0.001,0.003,0.001) # liczba infekcji
+  #p2<-0.75 # prawdopodobienstwo wykrycia
+  #n2 = log(1-P1)/log(1-r)
+  #p3<-0.8 # detection probability
+  #n3 = log(1-P2)/log(1-r)
+  #p4<-0.9
+  #n4 = log(1-P3)/log(1-r)
   
   #poziom inekcyjnosci wykrywany przy danej liczebosci proby
   #i danym prawdopodobienstwie wykrycia
-  n5<-seq(1:1000)
-  p5<-0.8
-  upInf<-(1-(nthroot((1-p5),n5)))*1000
-  p6<-0.95
-  upInf_2<-(1-(nthroot((1-p6),n5)))*1000
+  #n5<-seq(1:1000)
+  #p5<-0.8
+  #upInf<-(1-(nthroot((1-p5),n5)))*1000
+  #p6<-0.95
+  #upInf_2<-(1-(nthroot((1-p6),n5)))*1000
   
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    #x    <- faithful[, 2]
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  output$wykres <- renderPlot({
+    plot(pr()$pe1~pr()$ne1,
+         ylim = c(0,1),
+         xlab = 'Wielkość próby',
+         ylab = 'Prawdopodobieństwo wykrycia',
+         type = 'l')
   })
-
 })
--- ui.R
+++ ui.R
@@ -6,26 +6,32 @@ shinyUI(fluidPage(theme = 'bootstrap.css',
   # Sidebar with a slider input for number of bins
   sidebarLayout(position = 'left',
     sidebarPanel(
-      radioButtons(inputId = 'analyseType',
-                   label = 'Wybierz typ analizy',
-                   choices = list('Prawdopodobieństwo wykrycia' = "pVal",
-                                  'Min. wielkość próby' = 'mWP',
-                                  'Graniczny poziom infekcyjności' = 'gPI')
-                         ),
-      sliderInput(inputId = "probaB",
-                  label = "Prawdopodobieństwo wykrycia",
-                  min = 0.01,
-                  max = 1.00,
-                  value = 0.75),
+      #radioButtons(inputId = 'analyseType',
+       #            label = 'Wybierz typ analizy',
+        #           choices = list('Prawdopodobieństwo wykrycia' = "pVal",
+         #                         'Min. wielkość próby' = 'mWP',
+          #                        'Graniczny poziom infekcyjności' = 'gPI')
+           #              ),
+      #sliderInput(inputId = "probaB",
+      #            label = "Prawdopodobieństwo wykrycia",
+      #            min = 0.01,
+      #            max = 1.00,
+      #            value = 0.75),
       sliderInput(inputId = "infR",
                   label = "Poziom infekcji",
-                  min = 0.0001,
-                  max = 0.50,
-                  value = 0.01),
-      numericInput(InputId = 'minNumber',
-                   label = 'Minimalna liczebność próby', min = 200),
-      numericInput(InputId = 'maxNumber',
-                   label = 'Maksymalna liczebność próby', max = 4000),
+                  min = 0,
+                  max = 0.05,
+                  value = 0.01,
+                  step = 0.0005),
+      #sliderInput(inputId = "czT",
+      #            label = "wykrywalność w teście",
+      #            min = 0.6,
+      #            max = 1,
+      #            value = 0.9),
+      numericInput(inputId = 'minNumber',
+                   label = 'Minimalna liczebność próby', val = 200, min = 0),
+      numericInput(inputId = 'maxNumber',
+                   label = 'Maksymalna liczebność próby', val = 600, max = 3600),
       
     br(),
     p('Aplikacja zbudowana w', 
@@ -38,8 +44,8 @@ shinyUI(fluidPage(theme = 'bootstrap.css',
     # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(
-      tabPanel(title = 'Tabela', tableOutput('no_name_yet2')),
-      tabPanel(title = "Wykres", plotOutput('no_name_yet')))
+      #tabPanel(title = 'tabela', tableOutput('no_name_yet2')),
+      tabPanel(title = "Wykres", plotOutput(outputId = 'wykres', height = '600px')))
     )
   )
 ))
